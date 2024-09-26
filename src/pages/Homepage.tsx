0f14dd4b43f01e40fe88ebd2e2ca9f96dd6f5828
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-09-26 22:06:23 +0000

    fix: adding img

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -8,8 +8,12 @@ function Homepage() {
   };
 
   return (
-    <div className="flex flex-col gap-y-10 items-center">
-      <h1>Welcome to Shortcuts Quiz</h1>
+    <div className="flex flex-col gap-y-10 items-center p-5">
+      <h1>Welcome to Shortcuts.guru</h1>
+      <p>An interactive way to become a shortcut poweruser</p>
+
+      <img src="./logo.png" alt="" height="350" width="350" className="invert" />
+
       <button onClick={startQuiz}>Start a New Quiz</button>
     </div>
   );
