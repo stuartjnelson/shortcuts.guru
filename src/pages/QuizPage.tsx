c077a50d4a8bf20d8deef938bd6e7303a6e7df6f
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-09-26 21:45:19 +0000

    fix: working on quiz

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -16,11 +16,14 @@ function QuizPage() {
   const [pressedKeys, setPressedKeys] = useState<string[]>([]);
   const [keyPressIndex, setKeyPressIndex] = useState<number>(0); // Track the number of keys pressed
   const [usersAnswer, setUsersAnswer] = useState<string>("");
+  const [isUserAnswerCorrect, setIsUserAnswerCorrect] = useState<boolean>(false)
 
   useEffect(() => {
     if (appName !== "vscode") {
       navigate("/quiz/select");
     } else {
+      // @TODO: really should set this only once the quiz has started...
+      setIsUserAnswerCorrect(true)
       generateRandomQuestion();
     }
   }, [appName, navigate]);
@@ -32,6 +35,7 @@ function QuizPage() {
 
     setQuestion(randomQuestion);
     setIsKeyPressMode(shouldPressKeys);
+    setIsUserAnswerCorrect(false)
 
     // Resetting all user input values
     setPressedKeys([]);
@@ -40,17 +44,23 @@ function QuizPage() {
   };
 
   const handleKeyPress = (e: KeyboardEvent) => {
+    console.log(e.key)
     const key = getKeyString(e);
 
     // Check if the pressed key matches the current expected key in the sequence
     if (question && key === question.keys[keyPressIndex]) {
       setPressedKeys((prevKeys) => [...prevKeys, key]);
-      setKeyPressIndex((prevIndex) => prevIndex + 1);
+
+      // Only increiment if not on last key
+      if (keyPressIndex + 1 < question.keys.length) {
+        setKeyPressIndex((prevIndex) => prevIndex + 1);
+      }
 
       // If all keys are pressed, check if the answer is correct
       if (keyPressIndex + 1 === question.keys.length) {
         alert("Correct!");
-        generateRandomQuestion(); // Move to next question
+        setIsUserAnswerCorrect(true);
+        // generateRandomQuestion(); // Move to next question
       }
     } else {
       // If the wrong key is pressed, reset and notify the user
@@ -61,7 +71,7 @@ function QuizPage() {
   };
 
   useEffect(() => {
-    if (isKeyPressMode) {
+    if (isKeyPressMode && !isUserAnswerCorrect) {
       window.addEventListener("keydown", handleKeyPress);
       return () => window.removeEventListener("keydown", handleKeyPress);
     }
@@ -84,32 +94,42 @@ function QuizPage() {
     const isQuestionCorrect:boolean = question?.answers.includes(usersAnswer.toLocaleLowerCase()) || false
 
     if (isQuestionCorrect) {
-      generateRandomQuestion();
+      // generateRandomQuestion();
+      setIsUserAnswerCorrect(true);
+      alert("Correct!");
     } else {
       // @TODO: Make this reuable not hard coded
       alert("Try again")
     }
   }
 
+  const nextQuestion = () => {
+    if (isUserAnswerCorrect) {
+      generateRandomQuestion()
+    } else {
+      alert("You must get this question correct before moving on")
+    }
+  }
+
   return (
-    <div>
+    <div className="flex flex-col gap-y-10 items-center">
       <h1>{appName} Quiz</h1>
       {question ? (
         <>
           {isKeyPressMode ? (
-            <div>
+            <div className="flex flex-col gap-y-5 items-center">
               <h2>Press this shortcut: {question.description}</h2>
               <p>Keys pressed: {pressedKeys.join(" + ")}</p>
               <p>Waiting for key {keyPressIndex + 1} of {question.keys.length}</p>
             </div>
           ) : (
-            <form>
+            <form className="flex flex-col gap-y-5 items-center">
               <h2>What's this shortcut?</h2>
               <p>{question.keys.join(" + ")}</p>
 
               <br />
 
-              <label>
+              <label className="flex flex-col">
                 Answer
 
                 <input 
@@ -126,7 +146,7 @@ function QuizPage() {
 
           {/* @TODO: Ensure questions has been answered before moving on */}
           {/* @TODO: Make this a link? */}
-          {/* <button onClick={generateRandomQuestion}>Next Question</button> */}
+          <button onClick={nextQuestion}>Next Question</button>
         </>
       ) : (
         <p>Loading...</p>
