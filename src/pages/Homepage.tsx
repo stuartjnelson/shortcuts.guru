a9a40a2ab0233a41837d0f483ef0ebfbf5dce30d
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-09-26 21:45:42 +0000

    fix: adding some basic styling

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Homepage() {
@@ -9,7 +8,7 @@ function Homepage() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col gap-y-10 items-center">
       <h1>Welcome to Shortcuts Quiz</h1>
       <button onClick={startQuiz}>Start a New Quiz</button>
     </div>
--- src/pages/SelectionPage.tsx
+++ src/pages/SelectionPage.tsx
@@ -10,7 +10,7 @@ function SelectionPage() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col gap-y-10 items-center">
       <h1>Select an App for the Quiz</h1>
       <button onClick={() => selectApp("vscode")}>Visual Studio Code</button>
       {/* Add more apps here in the future */}
