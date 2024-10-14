aa8376ef45f2d0afbb61bb8bd804745dd4f95f07
Stuart Nelson <stuartjnelson@users.noreply.github.com>
2024-10-14 20:44:56 +0000

    feat: adding some global settings

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import SelectionPage from "./pages/SelectionPage";
 import QuizPage from "./pages/QuizPage";
 import DefaultLayout from "./layouts/DefaultLayout";
 import QuizSettings from "./pages/QuizSettings";
+import { QuizSettingsProvider } from "./contexts/QuizSettingsContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* <Route path="/" element={<Homepage />} />
-        <Route path="/quiz/select" element={<SelectionPage />} />
-        <Route path="/quiz/:appName/new" element={<QuizPage />} /> */}
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Homepage />} />
+    <QuizSettingsProvider>
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<Homepage />} />
           <Route path="/quiz/select" element={<SelectionPage />} />
-          <Route path="/quiz/:appName/settings" element={<QuizSettings />} />
-          <Route path="/quiz/:appName/new" element={<QuizPage />} />
-        </Route>
-      </Routes>
-    </Router>
+          <Route path="/quiz/:appName/new" element={<QuizPage />} /> */}
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/quiz/select" element={<SelectionPage />} />
+            <Route path="/quiz/:appName/settings" element={<QuizSettings />} />
+            <Route path="/quiz/:appName/new" element={<QuizPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </QuizSettingsProvider>
   );
 }
 
